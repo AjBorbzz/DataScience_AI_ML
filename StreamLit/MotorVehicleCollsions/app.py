86d226b0e5f0a81987fdb0fc22e543d66d6bd9e2
AjBorbzz <ajborbeheaddhz@yahoo.com>
2024-04-28 19:16:56 +0800

    Added MotorVehicleCollision app

--- StreamLit/MotorVehicleCollsions/app.py
+++ StreamLit/MotorVehicleCollsions/app.py
@@ -4,7 +4,7 @@ import numpy as np
 import pydeck as pdk
 import plotly.express as px
 
-file = 'Motor_Vehicle_Collisions_-_Crashes.csv'
+file = 'Motor_Vehicle_Collisions_slim_version.csv'
 
 st.title("Motor Vehicle Collisions in New York City")
 st.markdown("This application is a Streamlit dashboard that can be used to analyze motor vehicle collisions in NYC")

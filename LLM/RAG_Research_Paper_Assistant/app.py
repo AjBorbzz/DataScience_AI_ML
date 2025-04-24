37ab25de4d7b3a8db15221a7acfc1e9cd28af787
AjBorbzz <ajborbeheaddhz@yahoo.com>
2025-04-24 11:03:32 +0800

    Done with necessary functions

--- LLM/RAG_Research_Paper_Assistant/app.py
+++ LLM/RAG_Research_Paper_Assistant/app.py
@@ -21,9 +21,54 @@ client = chromadb.PersistentClient(path="chroma_db")
 text_embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
 arxiv_tool = ArxivQueryRun()
 
+def extract_text_from_pdfs(uploaded_files):
+    all_text = ""
+    for uploaded_file in uploaded_files:
+        reader = PyPDF2.PdfReader(uploaded_file)
+        for page in reader.pages:
+            all_text += page.extract_text() or ""
+    return all_text
+
+def process_text_and_store(all_text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50, separators=["\n\n","\n"," ", ""])
+    chunks = text_splitter.split_text(all_text)
+    try:
+        client.delete_collection(name="knowledge_base")
+    except Exception:
+        pass
+
+    collection = client.create_collection(name="knowledge_base")
+
+    for i,chunk in enumerate(chunks):
+        embedding = text_embedding_model.encode(chunk)
+        collection.add(
+            ids=[f"chunk_{i}"],
+            embeddings=[embedding.tolist()]
+            metadata=[{"source": "pdf", "chunk_id": i}],
+            documents=[chunk]
+        )
+    return collection
+
+def semantic_search(query, collection, top_k=2):
+    query_embedding = text_embedding_model.encode(query)
+    results = collection.query(
+        query_embedding=[query_embedding.tolist()], n_results=top_k
+    )
+    return results
+
+def generate_response(query, context):
+    prompt = f"Query: {query}\nContext: {context}\nAnswer: "
+    response = completion(
+        model="gemini/gemini-1.5-flash",
+        messages=[{"content": prompt, "role": "user"}]
+        api_key=gemini_api
+    )
+    return response['choices'][0]['message']['content']
+
 
 def main():
-    pass
+    st.title("RAG-powered Research Paper Assistant")
+    
 
 if __name__ == "__main__":
     main()

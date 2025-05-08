656a77a56ce53ba11c2504569956846b9c59e4c0
AjBorbzz <ajborbeheaddhz@yahoo.com>
2025-05-08 21:38:53 +0800

    Replaced deprecated libraries of scikitlearn with new libraries

--- StreamLit/BinaryClassificationApp/app.py
+++ StreamLit/BinaryClassificationApp/app.py
@@ -7,7 +7,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 # from sklearn.metrics import plot_confusion_matrix, plot_roc_curve, plot_precision_recall_curve
-from sklearn.metrics import ConfusionMatrixDisplay
+from sklearn.metrics import ConfusionMatrixDisplay, RocCurveDisplay, PrecisionRecallDisplay
 from sklearn.metrics import precision_score, recall_score 
 
 def main():

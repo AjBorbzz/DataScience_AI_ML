beba69d2fbacbdf263ed404dfa2c54f62253e161
AjBorbzz <ajborbeheaddhz@yahoo.com>
2025-05-16 20:29:28 +0800

    Added Phishing Email Detection using LLM

---
+++ LLM/Phishing_Detection_System/phish_email_detection.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv
+import anthropic 
+
+
+load_dotenv()
+
+claude_api = os.getenv("ANTHROPIC_API_KEY")
+client = anthropic.Anthropic()
+
+message = client.messages.create(
+    model="claude-3-7-sonnet-20250219",
+    max_tokens=1000,
+    temperature=1,
+    system="You are a world-class poet. Respond only with short poems.",
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": "Why is the ocean salty?"
+                }
+            ]
+        }
+    ]
+)
+print(message.content)

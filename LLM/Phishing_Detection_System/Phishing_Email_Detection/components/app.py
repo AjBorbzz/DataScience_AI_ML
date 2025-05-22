326bb0b0d80e4cc17fb66aada6be7a7fe7619258
AjBorbzz <ajborbeheaddhz@yahoo.com>
2025-05-22 23:03:04 +0800

    Arranged main function for best practice

--- LLM/Phishing_Detection_System/Phishing_Email_Detection/components/app.py
+++ LLM/Phishing_Detection_System/Phishing_Email_Detection/components/app.py
@@ -3,15 +3,17 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import os
 from pathlib import Path
+from utils import logging_config
+import logging
+logger = logging.getLogger(__name__)
 from services.anthropic_client import process_phishing_detection
 from utils.parser import get_data_from_csv, extract_message_output
 from dotenv import load_dotenv
 
 load_dotenv()
 
-
-
 DATA_PATH = Path().resolve().parents[0] / "data"
+SAMPLE_DATA_FILE = "sample_data.csv"
 
 def main():
         # Get data from CSV
@@ -19,10 +21,23 @@ def main():
         #     print(data)
         #     data = parser.list_of_dicts_to_csv(data, "sample_data.csv")
 
-    data = get_data_from_csv(DATA_PATH / "sample_data.csv")
-    response = process_phishing_detection(data[3])
-    extracted_data = extract_message_output(response)
+    logger.info("Starting the phishing detection process.")
+    try:
+        csv_data = get_data_from_csv(DATA_PATH / SAMPLE_DATA_FILE)
 
+        if not csv_data:
+            logger.warning(f"No Data Found in {DATA_PATH / SAMPLE_DATA_FILE}. Exiting...")
+            return
+        
+        # response = process_phishing_detection(csv_data[3])
+        # extracted_data = extract_message_output(response)
+    except FileNotFoundError:
+        logger.error(f"Error: CSV file not found at {DATA_PATH / SAMPLE_DATA_FILE}")
+
+    except Exception as e:
+        logger.error(f"An unexpected error has occured : {e}", exc_info=True)
+
+    logger.info("Phishing Detection Process Completed.")
 
 
 if __name__ == '__main__':

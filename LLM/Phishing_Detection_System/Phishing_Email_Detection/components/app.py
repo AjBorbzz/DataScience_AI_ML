9386fd75babc47cd8319fbf361f387fc1156dc89
AjBorbzz <ajborbeheaddhz@yahoo.com>
2025-05-22 22:48:39 +0800

    Restructured app.py

--- LLM/Phishing_Detection_System/Phishing_Email_Detection/components/app.py
+++ LLM/Phishing_Detection_System/Phishing_Email_Detection/components/app.py
@@ -5,20 +5,22 @@ from pathlib import Path
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from services import anthropic_client
-from utils import ioc_utils, parser
+from services.anthropic_client import process_phishing_detection
+from utils.parser import get_data_from_csv, extract_message_output
 
 DATA_PATH = Path().resolve().parents[0] / "data"
 
 
-
-
 def main():
-    # Get data from CSV
-    data = parser.get_data_from_csv(DATA_PATH / "test.csv")
-    print(data)
-#     anthropic_client.process_phishing_detection(data)
-    
+        # Get data from CSV
+        #     data = parser.get_data_from_csv(DATA_PATH / "test.csv")
+        #     print(data)
+        #     data = parser.list_of_dicts_to_csv(data, "sample_data.csv")
+
+    data = get_data_from_csv(DATA_PATH / "sample_data.csv")
+    response = process_phishing_detection(data[3])
+    extracted_data = extract_message_output(response)
+
 
 
 if __name__ == '__main__':

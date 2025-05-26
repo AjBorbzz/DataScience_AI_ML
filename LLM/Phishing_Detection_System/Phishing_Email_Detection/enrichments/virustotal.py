282ea97b6c36b498a0db56131cea43b0fa05bdea
AjBorbzz <ajborbeheaddhz@yahoo.com>
2025-05-26 22:26:37 +0800

    Added virustotal api

--- LLM/Phishing_Detection_System/Phishing_Email_Detection/enrichments/virustotal.py
+++ LLM/Phishing_Detection_System/Phishing_Email_Detection/enrichments/virustotal.py
@@ -1,9 +1,37 @@
 import requests
 import os
+from dotenv import load_dotenv
 
-def check_virustotal(url):
-    headers = {"x-apikey": os.getenv("VT_API_KEY")}
-    response = requests.get(f"https://www.virustotal.com/api/v3/urls/{url}", headers=headers)
-    data = response.json()
-    positives = data['data']['attributes']['last_analysis_stats']['malicious']
-    return {"source": "VirusTotal", "positives": positives, "score": min(positives * 20, 100)}
+load_dotenv()
+
+VT_API_KEY = os.getenv("VIRUSTOTAL_API_KEY")
+VT_URL = "https://www.virustotal.com/api/v3/urls"
+
+def vt_scan_url(url):
+    payload = {"url": url}
+    headers = {
+        "accept": "application/json",
+        "x-apikey": VT_API_KEY,
+        "content-type": "application/x-www-form-urlencoded"
+    }
+
+    response = requests.post(VT_URL, data=payload, headers=headers)
+    return response.text
+
+def vt_get_url_report(analysis_id):
+    analysis_id = analysis_id.split("-")[1]
+    headers = {"accept": "application/json", "x-apikey": VT_API_KEY,}
+    response = requests.get(f"{VT_URL}/{analysis_id}", headers=headers)
+    # print(response.text)
+    return response.text
+
+
+
+def main():
+    res = vt_scan_url("www.netflix.com")
+    analysis_id = res.get("data").get("id")
+    url_resport = vt_get_url_report(analysis_id)
+    print(url_resport)
+
+if __name__ == "__main__":
+    main()

f48167af033a3a06ec5e6839fdab02607d19af20
AjBorbzz <ajborbeheaddhz@yahoo.com>
2025-05-28 21:48:36 +0800

    Added task to agents

--- MultiAgent/MultiAgent_Resume_Reviewer/app.py
+++ MultiAgent/MultiAgent_Resume_Reviewer/app.py
@@ -98,3 +98,117 @@ profiler = Agent(
         "groundwork for personalized resume enhancements."
     )
 )
+
+
+# Agent 3: Resume Strategist
+resume_strategist = Agent(
+    role="Resume Strategist for Engineers",
+    goal="Find all the best ways to make a "
+         "resume stand out in the job market.",
+    tools = [scrape_tool, search_tool,
+             read_resume, semantic_search_resume],
+    verbose=True,
+    llm=llm,
+    backstory=(
+        "With a strategic mind and an eye for detail, you "
+        "excel at refining resumes to highlight the most "
+        "relevant skills and experiences, ensuring they "
+        "resonate perfectly with the job's requirements."
+    )
+)
+
+
+# Agent 4: Interview Coach
+interview_preparer = Agent(
+    role="Engineering Interview Coach",
+    goal="Create interview questions and talking points "
+         "based on the resume and job requirements",
+    tools = [scrape_tool, search_tool,
+             read_resume, semantic_search_resume],
+    verbose=True,
+    llm=llm,
+    backstory=(
+        "Your role is crucial in anticipating the dynamics of "
+        "interviews. With your ability to formulate key questions "
+        "and talking points, you prepare candidates for success, "
+        "ensuring they can confidently address all aspects of the "
+        "job they are applying for."
+    )
+)
+
+
+# Task for Researcher Agent: Extract Job Requirements
+research_task = Task(
+    description=(
+        "Analyze the job posting URL provided ({job_posting_url}) "
+        "to extract key skills, experiences, and qualifications "
+        "required. Use the tools to gather content and identify "
+        "and categorize the requirements."
+    ),
+    expected_output=(
+        "A structured list of job requirements, including necessary "
+        "skills, qualifications, and experiences."
+    ),
+    agent=researcher,
+    async_execution=True
+)
+
+
+# Task for Profiler Agent: Compile Comprehensive Profile
+profile_task = Task(
+    description=(
+        "Compile a detailed personal and professional profile "
+        "using the GitHub ({github_url}) URLs, and linkedin profile "
+        "({linkedin_profile}). Utilize tools to extract and "
+        "synthesize information from these sources."
+    ),
+    expected_output=(
+        "A comprehensive profile document that includes skills, "
+        "project experiences, contributions, interests, and "
+        "communication style."
+    ),
+    agent=profiler,
+    async_execution=True
+)
+
+
+# Task for Resume Strategist Agent: Align Resume with Job Requirements
+resume_strategy_task = Task(
+    description=(
+        "Using the profile and job requirements obtained from "
+        "previous tasks, tailor the resume to highlight the most "
+        "relevant areas. Employ tools to adjust and enhance the "
+        "resume content. Make sure this is the best resume even but "
+        "don't make up any information. Update every section, "
+        "inlcuding the initial summary, work experience, skills, "
+        "and education. All to better reflrect the candidates "
+        "abilities and how it matches the job posting."
+    ),
+    expected_output=(
+        "An updated resume that effectively highlights the candidate's "
+        "qualifications and experiences relevant to the job."
+    ),
+    output_file="tailored_resume.md",
+    context=[research_task, profile_task],
+    agent=resume_strategist
+)
+
+
+# Task for Interview Preparer Agent: Develop Interview Materials
+interview_preparation_task = Task(
+    description=(
+        "Create a set of potential interview questions and talking "
+        "points based on the tailored resume and job requirements. "
+        "Utilize tools to generate relevant questions and discussion "
+        "points. Make sure to use these question and talking points to "
+        "help the candiadte highlight the main points of the resume "
+        "and how it matches the job posting."
+    ),
+    expected_output=(
+        "A document containing key questions and talking points "
+        "that the candidate should prepare for the initial interview."
+    ),
+    output_file="interview_materials.md",
+    context=[research_task, profile_task, resume_strategy_task],
+    agent=interview_preparer
+)

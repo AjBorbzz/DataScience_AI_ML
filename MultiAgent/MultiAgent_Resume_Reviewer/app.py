26c095ecf30419a6e7ae7a0f29c13eb22e302677
AjBorbzz <ajborbeheaddhz@yahoo.com>
2025-05-28 21:47:41 +0800

    from notebook to app.py

--- MultiAgent/MultiAgent_Resume_Reviewer/app.py
+++ MultiAgent/MultiAgent_Resume_Reviewer/app.py
@@ -79,3 +79,22 @@ researcher = Agent(
         "effective application tailoring."
     )
 )
+
+
+# Agent 2: Profiler
+profiler = Agent(
+    role="Personal Profiler for Engineers",
+    goal="Do increditble research on job applicants "
+         "to help them stand out in the job market",
+    tools = [scrape_tool, search_tool,
+             read_resume, semantic_search_resume],
+    verbose=True,
+    llm=llm,
+    backstory=(
+        "Equipped with analytical prowess, you dissect "
+        "and synthesize information "
+        "from diverse sources to craft comprehensive "
+        "personal and professional profiles, laying the "
+        "groundwork for personalized resume enhancements."
+    )
+)

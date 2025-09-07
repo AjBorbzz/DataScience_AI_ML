cedc0d9fdff5eb8ee5ea247e7407a722d185a71f
AjBorbzz <ajborbeheaddhz@yahoo.com>
2025-09-07 15:16:42 +0800

    added save index and chunk text in rag.py

---
+++ LLM/FastAPI_LLM/app/services/rag.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+from typing import List, Tuple
+import pickle
+import faiss
+import numpy as np
+from pypdf import PdfReader
+from app.core.config import settings
+from sentence_transformers import SentenceTransformer
+
+
+_model: SentenceTransformer | None = None
+_index: faiss.IndexFlatIP | None = None
+_docs: List[str] = []
+
+
+def _ensure_models_loaded()-> None:
+    global _models, _index, _docs
+    if _model is None:
+        _model = SentenceTransformer("sentence-transofmers/all-MiniLM-L6-v2")
+    if _index is None:
+        if settings.FAISS_INDEX_PATH.exists() and settings.DOCS_METADATA_PATH.exists():
+            _index = faiss.read_index(str(settings.FAISS_INDEX_PATH))
+            with open(settings.DOCS_METADATA_PATH, "rb") as f:
+                _docs = pickle.load(f)
+
+        else:
+            _index = faiss.IndexFlatIP(384)
+            _docs = []
+
+def _save_index() -> None:
+    faiss.write_index(_index, str(settings.FAISS_INDEX_PATH))
+    with open(settings.DOCS_METADATA_PATH, "wb") as f:
+        pickle.dump(_docs, f)
+
+
+def _chunk_text(text: str, chunk_size: int, overlap: int)->List[str]:
+    chunks: List[str] = []
+    start = 0
+    while start < len(text):
+        end = min(start + chunk_size, len(text))
+        chunks.append(text[start: end])
+        start += chunk_size - overlap
+    return [c.strip() for c in chunks if c.strip()]
+
+

d5fc58bad5c8cb2b25bfd1caca21b2ef1c589761
AjBorbzz <ajborbeheaddhz@yahoo.com>
2026-05-30 03:14:57 +0800

    updated first prompt

--- LLM/AiSOCSummarizer/backend/app/prompts/best_practices.py
+++ LLM/AiSOCSummarizer/backend/app/prompts/best_practices.py
@@ -1,3 +1,12 @@
 SYSTEM = """
 You are a Security Architecture advisor reviewing a SOC incident.
 """
+
+def build_prompt(extracted_context: str, soc_narrative: str, dr_actions: str) -> str:
+    return f"""Based in this incident, recommend targeted security improvements. 
+            EXTRACTED CONTEXT: 
+             {extracted_context}
+
+            SOC A
+           """ 
+
